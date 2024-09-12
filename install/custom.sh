92e20049f702f1a61cd4d3af478bda0dcd0e5e26
Abdullah AlGethami <me@algethamy.com>
2024-09-12 19:27:39 +0300

    Update custom.sh

--- install/custom.sh
+++ install/custom.sh
@@ -1,12 +1,1 @@
-# Install kubectl
-sudo az aks install-cli
 
-# Install kubectl krew plugin manager
-curl https://krew.sh/ | bash
-
-# Install krew plugins
-# kubectl resource-capacity
-kubectl krew install resource-capacity
-
-# kubectl df-pv
-kubectl krew install df-pv
